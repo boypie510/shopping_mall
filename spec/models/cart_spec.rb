aa47cdc3fda73043868f037b93d77799598d42fc
Leo Li <boypie510@gmail.com>
2020-11-03 10:58:54 +0800

    pass third rspec

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -15,9 +15,20 @@ RSpec.describe Cart, type: :model do
 
       expect(cart.items.length).to be 3
       expect(cart.items.first.quantity).to be 3
-      expect(cart.items.first.quantity).to be 5
+      expect(cart.items.second.quantity).to be 5
     end
-    it "商品可以放到購物車裡，也可以再拿出來"
+    it "商品可以放到購物車裡，也可以再拿出來" do 
+      cart = Cart.new
+      p1 = Product.create(title: "iPhone")
+      p2 = Product.create(title: "iPad")
+
+      3.times { cart.add_item(p1.id) }
+      2.times { cart.add_item(p2.id) }
+
+      expect(cart.items.first.product_id).to be p1.id
+      expect(cart.items.second.product_id).to be p2.id
+      expect(cart.items.first.product).to be_a Product
+    end
     it "每個 Cart Item 都可以計算它自己的金額（小計）"
     it "可以計算整台購物車的總消費金額"
     it "特別活動可能可搭配折扣（例如聖誕節的時候全面打 9 折，或是滿額滿千送百）"
